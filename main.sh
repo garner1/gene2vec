5dd97308bc1266307c36c930f15c09714a468b2f
garner1 <silvano.garnerone@gmail.com>
2017-05-16 12:44:08 +0200

    use R to generate a gene model and produce fasta files for each gene exomes

---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+## For each entrez gene ID create a fasta file containing the list of exom sequences;
+## for this run the R script ~/Work/pipelines/geneModel/genomicRanges.R
+
+datadir=/home/garner1/Work/dataset/gene2vec/genedoc
+
+head "$datadir"/1.fasta
+
